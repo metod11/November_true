bd207e8d894a02b6484a5d268e1e9e3a6fa0c1c2
metod11 <55208832+metod11@users.noreply.github.com>
2019-11-21 22:28:10 +0300

    Add files via upload

---
+++ include/Methods/Adam/AdamW.h
@@ -0,0 +1,27 @@
+#pragma once
+
+/* Tools */
+//#include "Data.h"
+#include "Tools/math.hpp"
+#include "Tools/StopCondition.hpp"
+
+
+/**
+ *
+ * @struct Adam
+ * @brief Adaptive Moment Estimation method
+ * @param Adam::f
+ * Минимизируемая функция
+ * @param Adam::startPoint
+ * Точка старта минимизации
+ * @param Adam::parameters
+ * Параметры метода
+ * @param Adam::grad_accuracy
+ * Точность расчета градиента в методе
+ * @param Adam::iter_limit
+ * Максимальное число итераций метода
+ *
+*/
+
+//IterationData Adam(Function f, Vector startPoint, Vector parameters, Real grad_accuracy, int iter_lim);
+IterationData AdamW(Function f, Vector startPoint, const StopCondition& stop_condition = default_stop_condition);
